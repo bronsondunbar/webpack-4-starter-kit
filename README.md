32d65c78736513f3f755737a7a3e49d57484af16
Bronson Dunbar <bronson@bronsondunbar.com>
2018-07-25 19:01:32 +0200

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Webpack 4 Starter
+
+This can be used to start creating apps using Webpack 4.
+
+## Libraries included:
+
+- <a href="http://getbootstrap.com/" target="_blank">Bootstrap</a>
+- <a href="https://getuikit.com/" target="_blank">UIkit</a>
+- <a href="https://fontawesome.com/" target="_blank">Font Awesome</a>
--- src/app/app.js
+++ src/app/app.js
@@ -21,17 +21,17 @@ export default class App extends Component {
 
               <ul>
                 <li>
-                  <a href="" target="_blank">
+                  <a href="http://getbootstrap.com/" target="_blank">
                     Bootstrap
                   </a>
                 </li>
                 <li>
-                  <a href="" target="_blank">
+                  <a href="https://getuikit.com/" target="_blank">
                     UIkit
                   </a>
                 </li>
                 <li>
-                  <a href="" target="_blank">
+                  <a href="https://fontawesome.com/" target="_blank">
                     Font Awesome
                   </a>
                 </li>
@@ -45,62 +45,62 @@ export default class App extends Component {
 
               <ul>
                 <li>
-                  <a href="" target="_blank">
+                  <a href="https://www.npmjs.com/package/classnames" target="_blank">
                     classnames
                   </a>
                 </li>
                 <li>
-                  <a href="" target="_blank">
+                  <a href="https://www.npmjs.com/package/dompurify" target="_blank">
                     dompurify
                   </a>
                 </li>
                 <li>
-                  <a href="" target="_blank">
+                  <a href="https://www.npmjs.com/package/moment" target="_blank">
                     moment
                   </a>
                 </li>
                 <li>
-                  <a href="" target="_blank">
+                  <a href="https://www.npmjs.com/package/prop-types" target="_blank">
                     prop-types
                   </a>
                 </li>
                 <li>
-                  <a href="" target="_blank">
+                  <a href="https://www.npmjs.com/package/react-redux" target="_blank">
                     react-redux
                   </a>
                 </li>
                 <li>
-                  <a href="" target="_blank">
+                  <a href="https://www.npmjs.com/package/react-router-dom" target="_blank">
                     react-router-dom
                   </a>
                 </li>
                 <li>
-                  <a href="" target="_blank">
+                  <a href="https://www.npmjs.com/package/react-router-redux" target="_blank">
                     react-router-redux
                   </a>
                 </li>
                 <li>
-                  <a href="" target="_blank">
+                  <a href="https://www.npmjs.com/package/react-scripts" target="_blank">
                     react-scripts
                   </a>
                 </li>
                 <li>
-                  <a href="" target="_blank">
+                  <a href="https://www.npmjs.com/package/redux" target="_blank">
                     redux
                   </a>
                 </li>
                 <li>
-                  <a href="" target="_blank">
+                  <a href="https://www.npmjs.com/package/redux-logger" target="_blank">
                     redux-logger
                   </a>
                 </li>
                 <li>
-                  <a href="" target="_blank">
+                  <a href="https://www.npmjs.com/package/redux-promise" target="_blank">
                     redux-promise
                   </a>
                 </li>
                 <li>
-                  <a href="" target="_blank">
+                  <a href="https://www.npmjs.com/package/redux-thunk" target="_blank">
                     redux-thunk
                   </a>
                 </li>
