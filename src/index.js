a07ca08030b17369a92f214813c6246b8a66080a
Bronson Dunbar <bronson@bronsondunbar.com>
2018-07-26 16:25:11 +0200

    Updates

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import registerServiceWorker from "./registerServiceWorker"
-import { hot } from "react-hot-loader"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
+import { hot } from 'react-hot-loader'
 
-import App from "./app/app.js"
+import App from './app/app.js'
 
-import "./sass/style.scss"
+import './sass/style.scss'
 
 const AppWithHot = hot(module)(App)
 
-var mountNode = document.getElementById("app")
+var mountNode = document.getElementById('app')
 ReactDOM.render(<AppWithHot />, mountNode)
 registerServiceWorker()
